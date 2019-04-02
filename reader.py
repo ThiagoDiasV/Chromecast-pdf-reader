d79028e1e7d5c4e8f3e329bb86d64768cd998bf7
ThiagoDiasV <thiago76ers@gmail.com>
2019-04-01 21:26:26 -0300

    init

---
+++ reader.py
@@ -0,0 +1,10 @@
+from tika import parser
+import re
+
+pattern = re.compile(r'\d,\d{3}\s\d*\s\d?\d,\d{2}\s\d*\s\d?\d,\d{2}')
+
+raw = parser.from_file('pdf samples/Varfarina amostra 1 rep 1.pdf')
+text = raw['content']
+
+matches = re.findall(pattern, text)
+print(matches)
